58cfd0e1eca9dc750c52811ad7c7326bb46131f4
Anna <anek_ogonek@mail.ru>
2022-11-27 21:14:40 +0300

    Added requirements and decision for zadacha_4

---
+++ zadacha_4/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 4
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+Console.Write("Введите целое число 1: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число 2: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число 3: ");
+int number_3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number_1;
+
+if(number_2 > max)
+{
+    max = number_2;  
+}
+if(number_3 > max)
+{
+    max = number_3; 
+}
+Console.WriteLine($"Максимум = {max}");
