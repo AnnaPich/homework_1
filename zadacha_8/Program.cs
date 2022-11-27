88dd942b8e6f486c4674b2476cf71e80f1acf8ea
Anna <anek_ogonek@mail.ru>
2022-11-27 22:25:53 +0300

    Added requirements and decision for zadacha_8

---
+++ zadacha_8/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 8
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.Write("Введите натуральное число больше 1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number >= 2)
+{
+    int N = number % 2;
+    int count = 2;
+    if(N == 1)
+    {
+        while(count < number)
+        {
+            Console.Write($"{count}, ");
+            count = count + 2;
+        }
+    }
+    else  
+    {
+        while(count < number+1)
+        {
+            Console.Write($"{count}, ");
+            count = count + 2;
+        }
+    }
+}  
+else
+{
+    Console.Write("Ошибка! Введите натуральное число, больше 1");
+}
